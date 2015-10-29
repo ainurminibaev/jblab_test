3f27cc236bffdcea63b64e41d7e1ac1ffcc13bde
Roman Egorov <roma.romawka@gmail.com>
2015-10-29 18:00:58 +0300

    b34 added

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("sdfdsf");
-        int k = 0;
+        int k = 34;
         switch (k) {
             case 1:
-                System.out.println("Your name");
+                System.out.println("Roman");
+                break;
+            case 34:
+                System.out.println("34");
                 break;
             default:
                 System.out.println("Ainur");
