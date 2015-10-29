a8524eae4afd0bf830da9bf700fa09f790d2ca77
Ainur Minibaev <rapper01@list.ru>
2015-10-29 17:58:17 +0300

    b34

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("sdfdsf");
-        int k = 0;
+        int k = 34;
         switch (k) {
             case 1:
                 System.out.println("Your name");
                 break;
+            case 34:
+                System.out.println("34");
             default:
                 System.out.println("Ainur");
         }
