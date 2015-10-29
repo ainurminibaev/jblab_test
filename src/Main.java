45346dbccfa120f2add40bcf8e71c37d0b13ee76
mikl778890@yandex.ru <diman110011>
2015-10-29 17:55:10 +0300

    Misha 1.0

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("sdfdsf");
-        int k = 0;
+        int k = 2;
         switch (k) {
             case 1:
-                System.out.println("Your name");
+                System.out.println("MISHA");
                 break;
+            case 2:
+                System.out.print("KOROL");
             default:
                 System.out.println("Ainur");
         }
