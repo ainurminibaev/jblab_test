c4a0326b835caa240ffeec025cd157f5299c9a52
Ainur Minibaev <rapper01@list.ru>
2015-10-29 17:54:27 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by ainurminibaev on 29.10.15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("sdfdsf");
+        int k = 0;
+        switch (k) {
+            case 1:
+                System.out.println("Your name");
+                break;
+            default:
+                System.out.println("Ainur");
+        }
+    }
+}
