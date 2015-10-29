d63e2874bb362c72e7dd0a10dc682531956372ca
VladZakharov <vladzax95@gmail.com>
2015-10-29 17:56:21 +0300

    b78878

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("sdfdsf");
-        int k = 0;
+        int k = 78878;
         switch (k) {
             case 1:
-                System.out.println("Your name");
+                System.out.println("Vlad");
+                break;
+            case 78878:
+                System.out.println("78878");
                 break;
             default:
                 System.out.println("Ainur");
