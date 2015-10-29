88d8352b8bc0580acde9ec9fb9aa0f992867f664
mikl778890@yandex.ru <diman110011>
2015-10-29 18:13:34 +0300

    My Project 3.0

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("sdfdsf");
         int k = 2;
-        int k = 34;
         switch (k) {
             case 1:
                 System.out.println("MISHA");
-
                 break;
             case 2:
                 System.out.print("KOROL");
